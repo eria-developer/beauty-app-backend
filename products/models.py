cb4c6f78bb11243414f503d4dabfe022a6f7f147
eria-developer <eriddeveloper@gmail.com>
2024-09-16 11:13:45 +0300

    added image field to the categories

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
+    image = models.ImageField(upload_to="category_images/", null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
