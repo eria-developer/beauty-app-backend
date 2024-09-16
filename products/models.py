93c88be193d2e51c1cf515ed210724bf98f561bc
eria-developer <eriddeveloper@gmail.com>
2024-09-16 09:12:45 +0300

    added category model

--- products/models.py
+++ products/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.conf import settings
 
-PRODUCT_CATEGORIES = [
-    ("perfumes", "Perfumes"),
-    ("vaselines", "Vaselines"),
-    ("bodysprays", "Body Sprays"),
-]
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    category = models.CharField(choices=PRODUCT_CATEGORIES, max_length=20)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
