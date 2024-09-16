67350e79f597f34058f4aa8e55b4d55259794149
eria-developer <eriddeveloper@gmail.com>
2024-09-16 10:54:51 +0300

    solved a bug

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,5 +9,5 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Product
+        model = Category
         fields = '__all__' 
